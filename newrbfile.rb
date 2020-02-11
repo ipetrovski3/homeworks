4c511a330060c3ec2d1c2ee42c4857c6cb477364
ipetrovski3 <60879137+ipetrovski3@users.noreply.github.com>
2020-02-11 09:43:59 +0100

    Add files via upload

---
+++ newrbfile.rb
@@ -0,0 +1,15 @@
+# simple math calculation
+puts "What is 59/3?" , 59/3
+
+# comment to make something
+puts "Lets make it that number to floating point"
+
+# making variables
+puts x=59
+puts y=3 
+
+# converting variables in to floating points
+puts x.to_f/y.to_f
+
+# cheering on job done :)
+puts "Yesss i did it!"
